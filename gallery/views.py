d41bf841ee04b676835da1bf5f44e8ced94a3358
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-08 19:29:10 +0000

    added add image to gallery function

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Gallery
+from .forms import GalleryForm
 
 # Create your views here.
 def all_images(request):
@@ -11,3 +15,29 @@ def all_images(request):
     }
 
     return render(request, 'gallery/gallery.html', context)
+
+
+@login_required    
+def add_image(request):
+    """ Add a image to gallery """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site admin can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = GalleryForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added image!')
+            return redirect(reverse('gallery'))
+        else:
+            messages.error(request, 'Failed to add image. Please ensure the form is valid.')
+    else:
+        form = GalleryForm()
+        
+    template = 'gallery/add_image.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
