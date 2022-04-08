9be0f8e73f77963f68745411bc7556d4c6f61eb5
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-08 11:35:11 +0000

    added page unavailable message page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """A view to render index page"""
     return render(request, 'home/index.html')
+
+
+def maintenance(request):
+    """A view to render maintenance page"""
+    return render(request, 'home/maintenance.html')    
