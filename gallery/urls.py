2070392f9c52d30a7ba9acd80924fa18e9e8ed69
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-08 18:45:14 +0000

    added photos to gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery, name='gallery'),
+    path('', views.all_images, name='gallery'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Gallery
 
 # Create your views here.
+def all_images(request):
+    """ A view to show all images """
+    images = Gallery.objects.all()
 
-def gallery(request):
-    """A view to render gallery page"""
-    return render(request, 'gallery/gallery.html')
+    context = {
+        'images': images,
+    }
+
+    return render(request, 'gallery/gallery.html', context)
