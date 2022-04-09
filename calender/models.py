6188c305af0cc187c969c241bf8632293907ed23
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-09 14:41:03 +0000

    added calender events functionality

--- calender/models.py
+++ calender/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import date
+import datetime
 
 # Create your models here.
+
+class Member_status(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    location = models.TextField()
+    open_to = models.ForeignKey('Member_status', null=True, blank=True, on_delete=models.SET_NULL)
+    
+
+    def __str__(self):
+        return self.name
--- calender/views.py
+++ calender/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event, Member_status
 
 # Create your views here.
 
+
+
+
 def calender(request):
     """A view to render index page"""
-    return render(request, 'calender/calender.html')
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'calender/calender.html', context)
