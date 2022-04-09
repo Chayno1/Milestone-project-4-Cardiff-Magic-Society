6366a2404fb8fa0984203216978d5cdb91203f96
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-09 15:15:53 +0000

    added add_event.html and functionality

--- calender/urls.py
+++ calender/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.calender, name='calender'),
+    path('add/', views.add_event, name='add_event'),
 ]
--- calender/views.py
+++ calender/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Event, Member_status
-
+from .forms import EventForm
 # Create your views here.
 
 
@@ -16,3 +18,29 @@ def calender(request):
     }
 
     return render(request, 'calender/calender.html', context)
+
+
+@login_required    
+def add_event(request):
+    """ Add an event to calender """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site admin can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = EventForm(request.POST, request.FILES)
+        if form.is_valid():
+            event = form.save()
+            messages.success(request, 'Successfully added Event!')
+            return redirect(reverse('calender'))
+        else:
+            messages.error(request, 'Failed to add event. Please ensure the form is valid.')
+    else:
+        form = EventForm()
+        
+    template = 'calender/add_event.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
