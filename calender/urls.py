559ed22dfa7f938b1c088a2dff9b12964bbc7b9a
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-09 16:07:34 +0000

    added edit and delete buttons and functionality to calender

--- calender/urls.py
+++ calender/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.calender, name='calender'),
     path('add/', views.add_event, name='add_event'),
+    path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
+    path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
 ]
--- calender/views.py
+++ calender/views.py
@@ -44,3 +44,46 @@ def add_event(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_event(request, event_id):
+    """ Edit an event on the calender """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site admin can do that.')
+        return redirect(reverse('home'))
+
+    event = get_object_or_404(Event, pk=event_id)
+    if request.method == 'POST':
+        form = EventForm(request.POST, request.FILES, instance=event)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated event!')
+            return redirect(reverse('calender'))
+        else:
+            messages.error(request, 'Failed to update event. Please ensure the form is valid.')
+    else:
+        form = EventForm(instance=event)
+        messages.info(request, f'You are editing {event.name}')
+
+    template = 'calender/edit_event.html'
+    context = {
+        'form': form,
+        'event': event,
+        
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_event(request, event_id):
+    """ Delete an event from calender """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site admin can do that.')
+        return redirect(reverse('home'))
+
+    event = get_object_or_404(Event, pk=event_id)
+    event.delete()
+    messages.success(request, 'Event deleted!')
+    return redirect(reverse('calender'))
