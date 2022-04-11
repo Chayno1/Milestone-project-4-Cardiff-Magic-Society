21509c20151e18dfec1fc9745dcf7a4dac5854c4
Chayno1 <chazmaster71@hotmail.co.uk>
2022-04-11 21:05:28 +0000

    removed secret_key and set debug

--- cardiff_magic_society/settings.py
+++ cardiff_magic_society/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&2r@i2%eq*l!v^%w6hsiyiu!)5e6shq2e9i&#*35qba@xa7g^m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cardiff-magic-society.herokuapp', 'localhost']
 
